ceaaccfae43942e7b6a32885dc54eb59d11a9776
tommy <tommy@tommys-Mac.local>
2018-03-05 00:21:18 +0800

    add the struct demo to github

---
+++ object_demo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "this is the first demo for objected" << endl;
+    return 0;
+}
---
+++ struct_demo/struct_demo.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct Student {
+    int id;
+    char name[50];
+    int sex;
+    int age;
+} ;
+
+void print_info(Student stu) {
+    cout << "==============" << endl;
+    cout << "id is\t" << stu.id << endl;
+    cout << "name is\t" << stu.name << endl;
+
+    if (stu.sex == 0) {
+        cout << "sex is\t" << "girl" << endl;
+    } else {
+        cout << "sex is\t" << "boy" << endl;
+    }
+    cout << "age is\t" << stu.age << endl;
+}
+
+int main() {
+    Student stu1;
+    Student stu2;
+
+    stu1.id = 10001;
+    strcpy(stu1.name, "tommy");
+    stu1.sex = 1;
+    stu1.age = 28;
+
+    stu2.id = 10002;
+    strcpy(stu2.name, "baby");
+    stu2.sex = 0;
+    stu2.age = 22;
+
+    print_info(stu1);
+    print_info(stu2);
+    
+}
